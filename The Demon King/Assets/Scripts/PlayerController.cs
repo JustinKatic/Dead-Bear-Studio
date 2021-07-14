80994ef7950345a5e229d407fa94c497f248bab0
Justin <justinkatic4@gmail.com>
2021-07-14 16:30:49 +1000

    working on getting character in

--- The Demon King/Assets/Scripts/PlayerController.cs
+++ The Demon King/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using Cinemachine;
 
 public class PlayerController : MonoBehaviourPun
 {
@@ -10,8 +11,33 @@ public class PlayerController : MonoBehaviourPun
     public int id;
     private int curAttackerId;
 
-    public Rigidbody rig;
+    public float moveSpeed;
+    public float turnSpeed = 15;
+    Animator animator;
+    Camera mainCamera;
+
+    Vector2 input;
+
     public Player photonPlayer;
+
+
+    private void Update()
+    {
+        if (!photonView.IsMine)
+            return;
+
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+
+        //Set the animators blend tree to correct animation based of inputs, with 0.1 smooth added
+        animator.SetFloat("InputX", input.x, 0.1f, Time.deltaTime);
+        animator.SetFloat("InputY", input.y, 0.1f, Time.deltaTime);
+
+        //set the players rotation to the direction of the camera with a slerp smoothness
+        float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.deltaTime);
+
+    }
 
 
     [PunRPC]
@@ -25,11 +51,14 @@ public class PlayerController : MonoBehaviourPun
         // is this not our local player?
         if (!photonView.IsMine)
         {
-            GetComponentInChildren<Camera>().gameObject.SetActive(false);
-            rig.isKinematic = true;
+            Destroy(GetComponent<Rigidbody>());
+            Destroy(GetComponentInChildren<Camera>().gameObject);
+            Destroy(GetComponentInChildren<CinemachineFreeLook>().gameObject);
         }
         else
         {
+            animator = GetComponent<Animator>();
+            mainCamera = GetComponentInChildren<Camera>();
             //GameUI.instance.Initialize(this);
         }
     }
