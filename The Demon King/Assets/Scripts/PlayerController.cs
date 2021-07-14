5580c8282650e7ac0ec1e8058952d69a50ae315d
Justin <justinkatic4@gmail.com>
2021-07-15 00:57:36 +1000

    added jump and converted player to character controller, imported pro B

--- The Demon King/Assets/Scripts/PlayerController.cs
+++ The Demon King/Assets/Scripts/PlayerController.cs
@@ -12,11 +12,28 @@ public class PlayerController : MonoBehaviourPun
     public int id;
     private int curAttackerId;
 
-    public float moveSpeed;
+    public float jumpHeight;
+    public float gravity;
+
+    Vector3 velocity;
+    bool isJumping;
+    public float stepDown;
+    public float airControl;
+
+    private float jumpVelovity;
+
+    public float pushPower = 2f;
+
+
+    public float groundSpeed = 1f;
     public float turnSpeed = 15;
+    public float jumpDamp;
     Animator animator;
     Camera mainCamera;
-    CharacterController playerInput;
+    CharacterInputs CharacterInputs;
+    CharacterController cc;
+
+    Vector3 rootMotion;
 
 
     Vector2 input;
@@ -33,12 +50,11 @@ public class PlayerController : MonoBehaviourPun
             return;
         animator = GetComponent<Animator>();
 
-        animator.SetFloat("MoveSpeed", moveSpeed);
+        //animator.SetFloat("MoveSpeed", moveSpeed);
         mainCamera = Camera.main;
-        playerInput = new CharacterController();
+        CharacterInputs = new CharacterInputs();
         animator = GetComponent<Animator>();
     }
-
 
     private void Update()
     {
@@ -46,21 +62,69 @@ public class PlayerController : MonoBehaviourPun
             return;
 
         //Get value from input system for directional movement
-        input = playerInput.CharacterInputs.Move.ReadValue<Vector2>();
+        input = CharacterInputs.Player.Move.ReadValue<Vector2>();
 
+        //Set the animators blend tree to correct animation based of inputs, with 0.1 smooth added
+        animator.SetFloat("InputX", input.x, 0.1f, Time.deltaTime);
+        animator.SetFloat("InputY", input.y, 0.1f, Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
     }
     private void FixedUpdate()
     {
         if (!photonView.IsMine)
             return;
 
-        //Set the animators blend tree to correct animation based of inputs, with 0.1 smooth added
-        animator.SetFloat("InputX", input.x, 0.1f, Time.deltaTime);
-        animator.SetFloat("InputY", input.y, 0.1f, Time.deltaTime);
+        //Is in air state
+        if (isJumping)
+        {
+            UpdateInAir();
+        }
+        //IsGrounded state
+        else
+        {
+            UpdateOnGround();
+        }
 
         //set the players rotation to the direction of the camera with a slerp smoothness
         float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.deltaTime);
+
+    }
+
+    private void UpdateOnGround()
+    {
+        Vector3 stepForwardAmount = rootMotion * groundSpeed;
+        Vector3 stepDownAmount = Vector3.down * stepDown;
+
+        cc.Move(stepForwardAmount + stepDownAmount);
+        rootMotion = Vector3.zero;
+
+        if (!cc.isGrounded)
+        {
+            SetInAir(0);
+        }
+    }
+
+    private void UpdateInAir()
+    {
+        velocity.y -= gravity * Time.fixedDeltaTime;
+        Vector3 displacement = velocity * Time.fixedDeltaTime;
+        displacement += CalculateAirControl();
+        cc.Move(displacement);
+        isJumping = !cc.isGrounded;
+        rootMotion = Vector3.zero;
+        animator.SetBool("isJumping", isJumping);
+    }
+
+    private void OnAnimatorMove()
+    {
+        if (!photonView.IsMine)
+            return;
+        rootMotion += animator.deltaPosition;
     }
 
 
@@ -75,14 +139,15 @@ public class PlayerController : MonoBehaviourPun
         // is this not our local player?
         if (!photonView.IsMine)
         {
-            Destroy(GetComponent<Rigidbody>());
             Destroy(GetComponentInChildren<Camera>().gameObject);
             Destroy(GetComponentInChildren<CinemachineFreeLook>().gameObject);
         }
         else
         {
+            cc = GetComponent<CharacterController>();
             animator = GetComponent<Animator>();
             mainCamera = Camera.main;
+            jumpVelovity = Mathf.Sqrt(2 * gravity * jumpHeight);
             //GameUI.instance.Initialize(this);
         }
     }
@@ -90,14 +155,59 @@ public class PlayerController : MonoBehaviourPun
     private void OnEnable()
     {
         if (photonView.IsMine)
-            playerInput.CharacterInputs.Enable();
+            CharacterInputs.Player.Enable();
 
     }
 
     private void OnDisable()
     {
         if (photonView.IsMine)
-            playerInput.CharacterInputs.Disable();
+            CharacterInputs.Player.Disable();
+    }
+
+    void Jump()
+    {
+        if (!isJumping)
+        {
+            SetInAir(jumpVelovity);
+        }
+    }
+
+    private void SetInAir(float jumpVelocity)
+    {
+        isJumping = true;
+        velocity = animator.velocity * jumpDamp * groundSpeed;
+        velocity.y = jumpVelocity;
+        animator.SetBool("isJumping", true);
+    }
+
+
+    Vector3 CalculateAirControl()
+    {
+        return ((transform.forward * input.y) + (transform.right * input.x)) * (airControl / 100);
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        // no rigidbody
+        if (body == null || body.isKinematic)
+            return;
+
+        // We dont want to push objects below us
+        if (hit.moveDirection.y < -0.3f)
+            return;
+
+        // Calculate push direction from move direction,
+        // we only push objects to the sides never up and down
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+        // If you know how fast your character is trying to move,
+        // then you can also multiply the push velocity by that.
+
+        // Apply the push
+        body.velocity = pushDir * pushPower;
     }
 
 }
