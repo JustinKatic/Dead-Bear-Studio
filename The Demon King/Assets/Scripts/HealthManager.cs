6e637394e66f255b4f470f69b84edc8d0a29230a
Justin <justinkatic4@gmail.com>
2021-07-22 12:12:57 +1000

    fixed devour/stun functionality

--- The Demon King/Assets/Scripts/HealthManager.cs
+++ The Demon King/Assets/Scripts/HealthManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class HealthManager : MonoBehaviourPun
 {
-    
+
     [Header("HealthStats")]
     public int MaxHealth = 3;
     public int CurrentHealth = 0;
@@ -18,22 +18,24 @@ public class HealthManager : MonoBehaviourPun
 
     public bool Dead = false;
     protected int curAttackerId;
-    
+
     public TMP_Text OverheadText = null;
 
     public bool beingDevoured = false;
     public bool canBeDevoured = false;
 
-    
-    
-    
+    public bool isStunned = false;
+
+
+
+
     //Updates the players text to everyone on the server
     [PunRPC]
     public void UpdateOverheadText(string textToDisplay)
     {
         OverheadText.text = textToDisplay;
     }
-    
+
     [PunRPC]
     public void Heal(int amountToHeal)
     {
@@ -42,38 +44,44 @@ public class HealthManager : MonoBehaviourPun
         OverheadText.text = CurrentHealth.ToString();
         HealthRegenTimer = TimeBeforeHealthRegen;
     }
-    
-    
+
+
     //This is run when the player has been stunned
     [PunRPC]
     protected void Stunned()
     {
-        StartCoroutine(StunnedCorutine());
+        if (!isStunned)
+            StartCoroutine(StunnedCorutine());
 
         IEnumerator StunnedCorutine()
         {
-            StunnedBehaviour();
+            isStunned = true;
+            OnStunStart();
             //Things that affect everyone
             canBeDevoured = true;
             photonView.RPC("UpdateOverheadText", RpcTarget.All, "Stunned");
+            Debug.Log("stunned");
+
             yield return new WaitForSeconds(stunnedDuration);
-            DevourFinished();
+
+            isStunned = false;
+            OnStunEnd();
         }
     }
-    
+
     [PunRPC]
     public void TakeDamage(int attackerId, int damage)
     {
         if (beingDevoured)
             return;
-    
+
         CurrentHealth -= damage;
         curAttackerId = attackerId;
-    
+
         HealthRegenTimer = TimeBeforeHealthRegen;
-    
+
         photonView.RPC("UpdateOverheadText", RpcTarget.All, CurrentHealth.ToString());
-    
+
         //die if no health left
         if (CurrentHealth <= 0)
             photonView.RPC("Stunned", RpcTarget.All);
@@ -83,30 +91,30 @@ public class HealthManager : MonoBehaviourPun
     {
         if (beingDevoured)
             return;
-    
+
         CurrentHealth -= damage;
-    
+
         HealthRegenTimer = TimeBeforeHealthRegen;
-    
+
         photonView.RPC("UpdateOverheadText", RpcTarget.All, CurrentHealth.ToString());
-    
+
         //die if no health left
         if (CurrentHealth <= 0)
             photonView.RPC("Stunned", RpcTarget.All);
     }
 
-    protected virtual void StunnedBehaviour()
+    protected virtual void OnStunStart()
     {
-        
+
     }
 
     protected virtual void InterruptedDevour()
     {
-        
+
     }
 
-    protected virtual void DevourFinished()
+    protected virtual void OnStunEnd()
     {
-        
+
     }
 }
