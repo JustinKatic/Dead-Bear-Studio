1d75b95a2f3b463058677447a1d921ac60e37e45
Justin <justinkatic4@gmail.com>
2021-08-01 15:43:14 +1000

    added death ground to elrics scene

---
+++ The Demon King/Assets/Scripts/DeathGround.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class DeathGround : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+            other.gameObject.GetPhotonView().RPC("Respawn", RpcTarget.All);
+    }
+
+}
+
