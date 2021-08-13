a5dfa1bf9365dae00e6bbbeede7452fa98387dff
Justin <justinkatic4@gmail.com>
2021-08-13 11:32:20 +1000

    Testing Dev Branch push

--- The Demon King/Assets/Scripts/EvolutionSystem/ExperienceManager.cs
+++ The Demon King/Assets/Scripts/EvolutionSystem/ExperienceManager.cs
@@ -65,7 +65,6 @@ public class ExperienceManager : MonoBehaviourPun
 
     private void Start()
     {
-
         if (photonView.IsMine)
             evolutionManager.ChangeEvolution(evolutionManager.nextEvolution, false);
     }
