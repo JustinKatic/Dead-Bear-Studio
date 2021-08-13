aa8734b844b27c02d7434c3bc35809d379187382
Justin <justinkatic4@gmail.com>
2021-08-13 12:32:47 +1000

    testing fixxes to win condition

--- The Demon King/Assets/Scripts/Character/DemonKingEvolution.cs
+++ The Demon King/Assets/Scripts/Character/DemonKingEvolution.cs
@@ -15,11 +15,14 @@ public class DemonKingEvolution : MonoBehaviourPun
 
     private ExperienceManager experienceManager;
 
+    private PlayerController playerController;
+
     // Start is called before the first frame update
     void Start()
     {
         if (photonView.IsMine)
         {
+            playerController = GetComponent<PlayerController>();
             experienceManager = GetComponent<ExperienceManager>();
 
             Hashtable hash = new Hashtable();
@@ -41,7 +44,7 @@ public class DemonKingEvolution : MonoBehaviourPun
             }
             if (timeSpentAsDemonKing >= TimeRequiredToWin)
             {
-                photonView.RPC("PlayerWon", RpcTarget.All, photonView.ViewID);
+                photonView.RPC("PlayerWon", RpcTarget.All, playerController.id);
             }
         }
     }
@@ -49,7 +52,7 @@ public class DemonKingEvolution : MonoBehaviourPun
     [PunRPC]
     void PlayerWon(int WinnersID)
     {
-        Debug.Log("The Winner is: " + GameManager.instance.GetPlayer(WinnersID).GetComponent<PlayerController>().photonPlayer.NickName);
+        Debug.Log("The Winner is: " + GameManager.instance.GetPlayer(WinnersID).photonPlayer.NickName;
     }
 
     public void ChangeToTheDemonKing()
--- The Demon King/Assets/Scripts/LeaderboardManager.cs
+++ The Demon King/Assets/Scripts/LeaderboardManager.cs
@@ -64,6 +64,5 @@ public class LeaderboardManager : MonoBehaviourPun
         }
 
         LeaderBoardHUD.SetActive(true);
-
     }
 }
