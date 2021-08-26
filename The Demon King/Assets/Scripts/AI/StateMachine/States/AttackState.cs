05d6dadf9d6f79ac4911ddf6967bbb3e1e4006e3
Justin <justinkatic4@gmail.com>
2021-08-26 13:57:02 +1000

    AI Attack VFX

--- The Demon King/Assets/Scripts/AI/StateMachine/States/AttackState.cs
+++ The Demon King/Assets/Scripts/AI/StateMachine/States/AttackState.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class AttackState : State
 {
     public bool CanAttack = false;
-    [SerializeField] private GameObject meleeVFX;
-    
+    [SerializeField] private GameObject[] attackVFX;
+
     public override void RunCurrentState()
     {
         PlayAttackState();
@@ -18,18 +18,36 @@ public class AttackState : State
         var towardsPlayer = target.transform.position - minionTransform.position;
 
         minionTransform.rotation = Quaternion.RotateTowards(
-            minionTransform.rotation, 
-            Quaternion.LookRotation(towardsPlayer), 
+            minionTransform.rotation,
+            Quaternion.LookRotation(towardsPlayer),
             Time.deltaTime * 180
-        );        
-        
+        );
+
         agent.SetDestination(target.transform.position + Vector3.one);
-        
+
         if (CanAttack)
         {
-            //Playe Vfx here
-            target.GetComponent<PlayerHealthManager>().TakeDamage(1,0 );
+            target.GetComponent<PlayerHealthManager>().TakeDamage(1, 0);
+            int randVfx = Random.Range(0, attackVFX.Length);
+            AttackVFX(randVfx);
         }
     }
-    
+
+    void AttackVFX(int VfxToPlay)
+    {
+        photonView.RPC("PlayAttackVFX_RPC", RpcTarget.All, VfxToPlay);
+    }
+
+    [PunRPC]
+    void PlayAttackVFX_RPC(int VfxToPlay)
+    {
+        StartCoroutine(PlayVFX(VfxToPlay));
+    }
+
+    IEnumerator PlayVFX(int VfxToPlay)
+    {
+        attackVFX[VfxToPlay].SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        attackVFX[VfxToPlay].SetActive(false);
+    }
 }
