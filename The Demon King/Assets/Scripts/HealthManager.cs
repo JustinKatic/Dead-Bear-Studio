e8511a9a26f546acabb46f22b8e199ba6295d198
Justin <justinkatic4@gmail.com>
2021-10-29 10:57:00 +1100

    demon king score worth var

--- The Demon King/Assets/Scripts/HealthManager.cs
+++ The Demon King/Assets/Scripts/HealthManager.cs
@@ -22,6 +22,8 @@ public class HealthManager : MonoBehaviourPun
     [Header("Evolution Stats")]
     [HideInInspector] public float MyExperienceWorth;
     [HideInInspector] public int myScoreWorth;
+    [HideInInspector] public int myDemonKingScoreWorth;
+
 
     [HideInInspector] public MinionType MyMinionType;
 
