52a79173d6ad745996d527ba54b564a7306815ee
Kyle Gian <kyle.gian@hotmail.com>
2021-11-19 12:52:02 +1100

    Graphics buttons now display which is currently active

--- The Demon King/Assets/Scripts/PlayerSettings/GraphicsSettingsMenu.cs
+++ The Demon King/Assets/Scripts/PlayerSettings/GraphicsSettingsMenu.cs
@@ -9,6 +9,7 @@ public class GraphicsSettingsMenu : MonoBehaviour
 {
     [SerializeField] private TMP_Text FPSText;
     [SerializeField] private Slider FPSSlider;
+    [SerializeField] private List<Button> qualityButtons;
     [SerializeField] private RenderPipelineAsset[] Quality;
 
     // Start is called before the first frame update
@@ -17,6 +18,8 @@ public class GraphicsSettingsMenu : MonoBehaviour
         Application.targetFrameRate = PlayerPrefs.GetInt("FramesPerSecond", 300);
         QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("Quality", 2));
         QualitySettings.renderPipeline = Quality[PlayerPrefs.GetInt("Quality", 2)];
+
+        SetButtonsInteractable(PlayerPrefs.GetInt("Quality", 2));
 
         FPSSlider.minValue = 60f;
         FPSSlider.maxValue = 300f;
@@ -35,20 +38,33 @@ public class GraphicsSettingsMenu : MonoBehaviour
 
     public void SetQualityLow()
     {
+        SetButtonsInteractable(0);
         QualitySettings.SetQualityLevel(0);
         QualitySettings.renderPipeline = Quality[0];
         PlayerPrefs.SetInt("Quality", 0);
     }
     public void SetQualityMedium()
     {
+        SetButtonsInteractable(1);
         QualitySettings.SetQualityLevel(1);
         QualitySettings.renderPipeline = Quality[1];
         PlayerPrefs.SetInt("Quality", 1);
     }
     public void SetQualityHigh()
     {
+        SetButtonsInteractable(2);
         QualitySettings.SetQualityLevel(2);
         QualitySettings.renderPipeline = Quality[2];
         PlayerPrefs.SetInt("Quality", 2);
+    }
+
+    private void SetButtonsInteractable(int setButtonNonInteractable)
+    {
+        foreach (var button in qualityButtons)
+        {
+            button.interactable = true;
+        }
+
+        qualityButtons[setButtonNonInteractable].interactable = false;
     }
 }
--- The Demon King/Assets/Scripts/PlayerSettings/PlayerControlSettingsMenu.cs
+++ The Demon King/Assets/Scripts/PlayerSettings/PlayerControlSettingsMenu.cs
@@ -3,20 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerControlSettingsMenu : MonoBehaviour
 {
     private Slider sensitivitySlider;
-    
+    [SerializeField] private TMP_Text sliderText;
+
     private void Start()
     {
         sensitivitySlider = GetComponent<Slider>();
         sensitivitySlider.maxValue = 100;
         sensitivitySlider.value = PlayerPrefs.GetFloat("MouseSensitivity", 10);
+        sliderText.text = PlayerPrefs.GetFloat("MouseSensitivity", 10).ToString();
     }
 
     public void OnMouseSensitivityChange(Slider SensitivitySlider)
     {
         PlayerPrefs.SetFloat("MouseSensitivity", SensitivitySlider.value);
+        sliderText.text = PlayerPrefs.GetFloat("MouseSensitivity", 10).ToString();
     }
 }
